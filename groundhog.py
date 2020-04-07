9e4600f9fb105f3fbb2a89159f98976e01f2e2ca
leeaajean <62607440+leeaajean@users.noreply.github.com>
2020-04-07 16:42:21 +0200

    Add files via upload

--- groundhog.py
+++ groundhog.py
@@ -2,6 +2,9 @@
 
 import sys
 
+number = []
+weird = []
+
 def print_dash_h():
     print("SYNOPSIS")
     print("\t./groundhog period")
@@ -17,16 +20,48 @@ def check_arg(arg):
         exit(84)
     return (period)
 
-number = []
+def takeSecond(elem):
+    return elem[1]
+
+def takeThird(elem):
+    return elem[2]
+
+def calc_moyenne(a, period):
+    moyenne = 0
+    i = 0
+    while (i < period):
+        moyenne = moyenne + number[a + i]
+        i = i + 1
+    moyenne = moyenne / period
+    return (moyenne)
+
+def abberation(a, period, deviation):
+    i = 0
+    moyenne = calc_moyenne(a, period)
+
+    value = number[a + period - 1]
+    if value <= moyenne - deviation:
+        if value != 0:
+            percent = (moyenne - 2 * deviation) / value * 100
+        else:
+            percent = 3000000
+        weird.append([value, percent, 1])
+    elif value >= moyenne + deviation:
+        if moyenne + 2 * deviation != 0:
+            percent = value / (moyenne + 2 * deviation) * 100
+        else:
+            percent = 3000000
+        weird.append([value, percent, 1])
+    return (weird)
 
 def recup_input(period):
     data = []
     i = 0
-    # number = []
     difference = []
     old_ratio = 0
     nb_switch = 0
     switch = 0
+    deviation = 0
     while True:
         try:
             data.append(input())
@@ -35,8 +70,32 @@ def recup_input(period):
         if data[i] == "STOP":
             if len(data) < period:
                 exit(84)
-            # print("5 weirdest values are", sort_weird_values())
-            print("Global tendency switched %d times" % nb_switch)
+            try:
+                print("Global tendency switched %d times" % nb_switch)
+                weird.sort(key = takeThird, reverse = True)
+                final_weird = []
+                i = 0
+                while (weird[i][2] >= 2 and i < 5):
+                    final_weird.append(weird[i])
+                    i = i + 1
+                    weird = weird[i:]
+                final_weird.sort(key = takeSecond, reverse = True)
+                weird.sort(key = takeSecond, reverse = True)
+                j = 0
+                while (i < 5):
+                    final_weird.append(weird[j])
+                    j = j + 1
+                    i = i + 1
+                i = 0
+                print("5 weirdest values are [", end='')
+                while (i < 5):
+                    print(final_weird[i][0], end='')
+                    if (i != 4):
+                        print(", ", end='')
+                    i = i + 1
+                print("]")
+            except:
+                sys.exit(84)
             break
         else:
             try:
@@ -54,7 +113,9 @@ def recup_input(period):
                         switch = 0
                     old_ratio = ratio
                 if i >= period - 1:
-                    standard_deviation(period, i)
+                    deviation = standard_deviation(period, i)
+                    if len(number) > period:
+                        weird = abberation(len(number) - period, period, round(deviation,2))
                     if switch == 1:
                         print("\t\ta switch occurs")
                         nb_switch += 1
@@ -105,30 +166,7 @@ def standard_deviation(period, i):
         print("g=nan\tr=nan%%\ts=%0.2f" % result, end='')
     else:
         print("s=%0.2f" % result, end = '')
-        #     weird_values(number, period, i, result)
-
-# def takeSecond(elem):
-#     return (elem[1])
-
-# def sort_weird_values():
-#     values.sort(key= takeSecond)
-#     k = len(values) - 1
-#     length = len(values) - 5
-#     weird = []
-#     while k >= length:
-#         weird.append(values[k][0])
-#         k -= 1
-#     return (weird)
-
-# def weird_values(number, period, i, deviation):
-#     tab = number[i + 1-period:i+1]
-#     moyenne = sum(number[i + 1 - period: i +1]) / period
-#     bh = moyenne + (2 * deviation)
-#     bb = abs(moyenne - (2 * deviation))
-
-#     for l in range (len(number[i + 1-period:i+1])):
-#         calc = (tab[l] - bb) / (bh - bb)
-#         values.append([tab[l], calc])
+    return (result)
     
 def main():
     if sys.argv[1] == "-h":
